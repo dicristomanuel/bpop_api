723759c3538d5755c01e30bfaa88a59afdb529da
dicristomanuel <dicristomanuel@icloud.com>
2015-09-13 18:48:42 -0700

    working on stats page layout

--- config/routes.rb
+++ config/routes.rb
@@ -11,19 +11,3 @@ Rails.application.routes.draw do
   get '/stats/searchgroup/:bpopToken', to: 'stats#searchGroupFans'
 
 end
-
-# Prefix Verb  URI Pattern                                 Controller#Action
-# stats_topfan GET   /stats/topfan(.:format)                     stats#topfan
-# fbposts GET   /fbposts(.:format)                          fbposts#index
-#        POST  /fbposts(.:format)                          fbposts#create
-# fbpost GET   /fbposts/:id(.:format)                      fbposts#show
-#        PATCH /fbposts/:id(.:format)                      fbposts#update
-#        PUT   /fbposts/:id(.:format)                      fbposts#update
-# fblikes GET   /fblikes(.:format)                          fblikes#index
-# fblike GET   /fblikes/:id(.:format)                      fblikes#show
-# fbcomments GET   /fbcomments(.:format)                       fbcomments#index
-# fbcomment GET   /fbcomments/:id(.:format)                   fbcomments#show
-#        GET   /get-gender-percentage/:bpopToken(.:format) fbposts#get_overall_gender_percentage
-#        GET   /stats/topfan/:bpopToken(.:format)          stats#topfan
-#        GET   /stats/searchfan/:bpopToken(.:format)       stats#searchFan
-#        GET   /stats/searchgroup/:bpopToken(.:format)     stats#searchGroupFans
